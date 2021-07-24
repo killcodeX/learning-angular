68092ac457125a7e5e27aafb737ccf87b0b45367
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-24 17:39:44 +0530

    new component and css created

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent
   ],
   imports: [
     BrowserModule,
