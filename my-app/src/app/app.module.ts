98f4e67b645af78e86e62f45a6354aad306ae04d
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-24 17:47:01 +0530

    removed my component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
   ],
   imports: [
     BrowserModule,
