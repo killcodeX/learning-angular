c805af2c97e893125b453d0e5a5d329e5354ee63
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-08-05 20:30:11 +0530

    installed material angualr

--- my-app2/src/app/app.component.ts
+++ my-app2/src/app/app.component.ts
@@ -7,8 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app2';
+  name : String = '';
+  password: any = '';
+  phone: any = '';
+  terms: boolean = false;
 
-  getEvent(evt: any){
-    console.warn(evt)
+  getName(data: any){
+    this.name = data.target.value
   }
+
+  getPassword(data: any){
+    this.password = data.target.value
+  }
+
+  getPhone(data: any){
+    this.phone = data.target.value
+  }
+
+  getTerms(data: any){
+    this.terms = data.target.checked
+  }
+
+  printValues(event : any){
+    event.preventDefault();
+    console.log('name', this.name)
+    console.log('name', this.password)
+    console.log('name', this.phone)
+    console.log('name', this.terms)
+  }
+
 }
--- my-app2/src/app/app.module.ts
+++ my-app2/src/app/app.module.ts
@@ -4,16 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersModule } from './users/users.module';
+
+//import FormsModule here
+import { FormsModule } from '@angular/forms';
+
+// material angular
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UsersModule
+    UsersModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
